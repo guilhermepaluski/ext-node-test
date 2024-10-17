28fd3ef246e451094a59d71ffcb536c68f3c1bbb
guilherme.paluski <guilherme.paluski@catolicasc.edu.br>
2024-10-16 21:31:52 -0300

    teste

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,7 @@ import { ext } from '../src/index';
 
 describe('numeroPorExtenso', () => {
     test('deve retornar o número por extenso corretamente', () => {
-        expect(ext(0)).toBe('zero');
+        expect(ext(0)).toBe
+        ('zero');
     });
 });
